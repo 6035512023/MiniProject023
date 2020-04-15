150e05505ffb02609cc5e6081044bf6b25843f77
6035512023 <6035512023@psu.ac.th>
2020-04-15 23:28:59 +0700

    commit7

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,19 @@
 import React from 'react'
-
+import fire from '../config/fire'
 
 const Home = () => {
+
+    const logout = () => {
+
+        fire.auth().signOut();
+    }
 
 
     return(
         <div>
             Home Page
+            <div><button onClick={logout}>LOGOUT</button></div>
+            
         </div>
     )
 }
