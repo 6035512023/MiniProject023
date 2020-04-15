62c229eb22166ffd712977819bf4939dc79e09b7
6035512023 <6035512023@psu.ac.th>
2020-04-15 21:21:54 +0700

    commit6

--- src/App.js
+++ src/App.js
@@ -4,16 +4,52 @@ import Login from './components/Login';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
+import fire from './config/fire';
+import Home from './components/Home';
 
 class App extends Component{
+  constructor(props){
+
+    super(props)
+
+    this.state ={
+
+      user: {}
+    }
+  }
+
+  componentDidMount(){
+    this.authListrner()
+  }
+
+  authListrner(){
+    fire.auth().onAuthStateChanged((user) =>{
+
+      if(user){
+        this.setState({user})
+      }
+      else {
+        this.setState({ user: null})
+      }
+    })
+  }
 
   render(){
+    if(this.state.user == null){
 
     return(
       <div>
         <Login />
       </div>
     )
+    }
+    
+    return(
+      <div>
+        <Home />
+      </div>
+    )
+
   }
 }
 
