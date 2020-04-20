a37c026809a3c9d8c6c241ac0fa4f4186f99276f
6035512023 <6035512023@psu.ac.th>
2020-04-20 18:01:44 +0700

    commit12

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -1,37 +1,21 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
-import amber from '@material-ui/core/colors/purple';
-
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
-
-const accent = amber[500]; 
+import React, { Component } from "react";
+import {
+MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline,
+MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem
+} from "mdbreact";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const Bottom = () =>{
 
     
     return (
-      <React.Fragment>
-        <CssBaseline />
-        <AppBar position="relative">
-          <Toolbar>
-            <Typography variant="h4" color="accent" noWrap>
-            Covid-19 News
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </React.Fragment>
+        <div>
+        <MDBNavbar color="cyan" dark expand="md">
+        <MDBNavbarBrand>
+          <strong className="white-text">Covid-19 News</strong>
+        </MDBNavbarBrand>
+      </MDBNavbar>
+    </div>
     );
 
 }
